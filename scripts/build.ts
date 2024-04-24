a6d464ccd0cae574d2e6af0ff951b6ed28eeb9a3
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2024-04-24 09:44:34 +0200

    update featherjs peerDependencies

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,10 +1,10 @@
-import path from 'path';
-import { build } from 'rocket-punch';
-import packages from '../.packages.json';
+import path from 'path'
+import { build } from 'rocket-punch'
+import packages from '../.packages.json'
 
-const { $schema, ...entry } = packages;
+const { $schema, ...entry } = packages
 
-export const cwd = path.resolve(__dirname, '..');
+export const cwd = path.resolve(__dirname, '..')
 
 build({
   cwd,
@@ -14,58 +14,58 @@ build({
       dependencies = {},
       peerDependencies = {},
       peerDependenciesMeta = {},
-    } = computedPackageJson;
+    } = computedPackageJson
 
     if ('react' in dependencies) {
-      peerDependencies['react'] = dependencies['react'];
-      delete dependencies['react'];
+      peerDependencies['react'] = dependencies['react']
+      delete dependencies['react']
     }
 
     if ('react-dom' in dependencies) {
-      peerDependencies['react-dom'] = dependencies['react-dom'];
-      delete dependencies['react-dom'];
+      peerDependencies['react-dom'] = dependencies['react-dom']
+      delete dependencies['react-dom']
     }
 
     if ('react-router-dom' in dependencies) {
-      peerDependencies['react-router-dom'] = dependencies['react-router-dom'];
-      delete dependencies['react-router-dom'];
+      peerDependencies['react-router-dom'] = dependencies['react-router-dom']
+      delete dependencies['react-router-dom']
     }
 
     if ('styled-components' in dependencies) {
-      peerDependencies['styled-components'] = dependencies['styled-components'];
-      delete dependencies['styled-components'];
+      peerDependencies['styled-components'] = dependencies['styled-components']
+      delete dependencies['styled-components']
     }
 
     if ('rxjs' in dependencies) {
-      dependencies['rxjs'] = '^7.0.0';
+      dependencies['rxjs'] = '^7.0.0'
     }
 
     if ('@terra-money/feather.js' in dependencies) {
-      peerDependencies['@terra-money/feather.js'] = dependencies['@terra-money/feather.js'];
-      delete dependencies['@terra-money/feather.js'];
+      //peerDependencies['@terra-money/feather.js'] = dependencies['@terra-money/feather.js'];
+      delete dependencies['@terra-money/feather.js']
     }
 
     if ('@cosmjs/amino' in dependencies) {
-      peerDependencies['@cosmjs/amino'] = dependencies['@cosmjs/amino'];
-      delete dependencies['@cosmjs/amino'];
+      peerDependencies['@cosmjs/amino'] = dependencies['@cosmjs/amino']
+      delete dependencies['@cosmjs/amino']
     }
 
     if ('axios' in dependencies) {
-      peerDependencies['axios'] = dependencies['axios'];
-      delete dependencies['axios'];
+      peerDependencies['axios'] = dependencies['axios']
+      delete dependencies['axios']
     }
-    
+
     computedPackageJson.dependencies =
-      Object.keys(dependencies).length > 0 ? dependencies : undefined;
+      Object.keys(dependencies).length > 0 ? dependencies : undefined
 
     computedPackageJson.peerDependencies =
-      Object.keys(peerDependencies).length > 0 ? peerDependencies : undefined;
+      Object.keys(peerDependencies).length > 0 ? peerDependencies : undefined
 
     computedPackageJson.peerDependenciesMeta =
       Object.keys(peerDependenciesMeta).length > 0
         ? peerDependenciesMeta
-        : undefined;
+        : undefined
 
-    return computedPackageJson;
+    return computedPackageJson
   },
-});
+})
